05d61d735db5d7aee02c9f8b6bab7d5ead6276b5
Tamegoro <a@b.com>
2015-11-27 22:42:11 +0900

    no message

--- VerySimpleAnalog/VerySimpleAnalog_a.js
+++ VerySimpleAnalog/VerySimpleAnalog_a.js
@@ -3,19 +3,37 @@ function getConfigData() {
 	var radioHandDiamond = document.getElementById('radio-hand-diamond');
 	var radioHandTrapezoid = document.getElementById('radio-hand-trapezoid');
 	var radioHandRectangle = document.getElementById('radio-hand-rectangle');
+	var radioHandPentagon = document.getElementById('radio-hand-pentagon');
+	var radioHandRound = document.getElementById('radio-hand-round');
 	var radioNomal = document.getElementById('radio-nomal');
 	var radioInvert = document.getElementById('radio-invert');
 	var toggleShowSecondhand = document.getElementById('toggle-show-secondhand');
 	var toggleShowDate = document.getElementById('toggle-show-date');
+	var toggleShowNumber = document.getElementById('toggle-show-number');
+	var toggleShowIndex = document.getElementById('toggle-show-index');
+	var radioTapNone = document.getElementById('radio-tap-none');
+	var radioTapDigital = document.getElementById('radio-tap-digital');
+	var radioTapFlip = document.getElementById('radio-tap-flip');
+	var timeShift = document.getElementById('time-shift');
+	var toggleReversal = document.getElementById('toggle-reversal');
 
 	var options = {
 		'radio-hand-diamond': radioHandDiamond.checked,
 		'radio-hand-trapezoid': radioHandTrapezoid.checked,
 		'radio-hand-rectangle': radioHandRectangle.checked,
+		'radio-hand-pentagon': radioHandPentagon.checked,
+		'radio-hand-round': radioHandRound.checked,
 		'radio-nomal': radioNomal.checked,
 		'radio-invert': radioInvert.checked,
 		'toggle-show-secondhand': toggleShowSecondhand.checked,
-		'toggle-show-date': toggleShowDate.checked
+		'toggle-show-date': toggleShowDate.checked,
+		'toggle-show-number': toggleShowNumber.checked,
+		'toggle-show-index': toggleShowIndex.checked,
+		'radio-tap-none': radioTapNone.checked,
+		'radio-tap-digital': radioTapDigital.checked,
+		'radio-tap-flip': radioTapFlip.checked,
+		'time-shift': timeShift.value,
+		'toggle-reversal': toggleReversal.checked
 	};
 
 	// Save for next launch
@@ -23,10 +41,19 @@ function getConfigData() {
 	localStorage['radio-hand-diamond'] = options['radio-hand-diamond'];
 	localStorage['radio-hand-trapezoid'] = options['radio-hand-trapezoid'];
 	localStorage['radio-hand-rectangle'] = options['radio-hand-rectangle'];
+	localStorage['radio-hand-pentagon'] = options['radio-hand-pentagon'];
+	localStorage['radio-hand-round'] = options['radio-hand-round'];
 	localStorage['radio-nomal'] = options['radio-nomal'];
 	localStorage['radio-invert'] = options['radio-invert'];
 	localStorage['toggle-show-secondhand'] = options['toggle-show-secondhand'];
 	localStorage['toggle-show-date'] = options['toggle-show-date'];
+	localStorage['toggle-show-number'] = options['toggle-show-number'];
+	localStorage['toggle-show-index'] = options['toggle-show-index'];
+	localStorage['radio-tap-none'] = options['radio-tap-none'];
+	localStorage['radio-tap-digital'] = options['radio-tap-digital'];
+	localStorage['radio-tap-flip'] = options['radio-tap-flip'];
+	localStorage['time-shift'] = options['time-shift'];
+	localStorage['toggle-reversal'] = options['toggle-reversal'];
 
 //	console.log('Got options: ' + JSON.stringify(options));
 
@@ -82,20 +109,38 @@ buttonSave.addEventListener('click', function() {
 	var radioHandDiamond = document.getElementById('radio-hand-diamond');
 	var radioHandTrapezoid = document.getElementById('radio-hand-trapezoid');
 	var radioHandRectangle = document.getElementById('radio-hand-rectangle');
+	var radioHandPentagon = document.getElementById('radio-hand-pentagon');
+	var radioHandRound = document.getElementById('radio-hand-round');
 	var radioNomal = document.getElementById('radio-nomal');
 	var radioInvert = document.getElementById('radio-invert');
 	var toggleShowSecondhand = document.getElementById('toggle-show-secondhand');
 	var toggleShowDate = document.getElementById('toggle-show-date');
+	var toggleShowNumber = document.getElementById('toggle-show-number');
+	var toggleShowIndex = document.getElementById('toggle-show-index');
+	var radioTapNone = document.getElementById('radio-tap-none');
+	var radioTapDigital = document.getElementById('radio-tap-digital');
+	var radioTapFlip = document.getElementById('radio-tap-flip');
+	var timeShift = document.getElementById('time-shift');
+	var toggleReversal = document.getElementById('toggle-reversal');
 
 	// Load any previously saved configuration, if available
 	if(localStorage['flag_save'] == "1") {
 		radioHandDiamond.checked = JSON.parse(localStorage['radio-hand-diamond']);
 		radioHandTrapezoid.checked = JSON.parse(localStorage['radio-hand-trapezoid']);
 		radioHandRectangle.checked = JSON.parse(localStorage['radio-hand-rectangle']);
+		radioHandPentagon.checked = JSON.parse(localStorage['radio-hand-pentagon']);
+		radioHandRound.checked = JSON.parse(localStorage['radio-hand-round']);
 		radioNomal.checked = JSON.parse(localStorage['radio-nomal']);
 		radioInvert.checked = JSON.parse(localStorage['radio-invert']);
 		toggleShowSecondhand.checked = JSON.parse(localStorage['toggle-show-secondhand']);
 		toggleShowDate.checked = JSON.parse(localStorage['toggle-show-date']);
+		toggleShowNumber.checked = JSON.parse(localStorage['toggle-show-number']);
+		toggleShowIndex.checked = JSON.parse(localStorage['toggle-show-index']);
+		radioTapNone.checked = JSON.parse(localStorage['radio-tap-none']);
+		radioTapDigital.checked = JSON.parse(localStorage['radio-tap-digital']);
+		radioTapFlip.checked = JSON.parse(localStorage['radio-tap-flip']);
+		timeShift.value = localStorage['time-shift'];
+		toggleReversal.checked = JSON.parse(localStorage['toggle-reversal']);
 	}
 
 })();
--- VerySimpleAnalog/VerySimpleAnalog_bc.js
+++ VerySimpleAnalog/VerySimpleAnalog_bc.js
@@ -3,6 +3,8 @@ function getConfigData() {
 	var radioHandDiamond = document.getElementById('radio-hand-diamond');
 	var radioHandTrapezoid = document.getElementById('radio-hand-trapezoid');
 	var radioHandRectangle = document.getElementById('radio-hand-rectangle');
+	var radioHandPentagon = document.getElementById('radio-hand-pentagon');
+	var radioHandRound = document.getElementById('radio-hand-round');
 	var colorHourhand = document.getElementById('color-hourhand');
 	var colorHourhandOutline = document.getElementById('color-hourhand-outline');
 	var colorMinutehand = document.getElementById('color-minutehand');
@@ -17,11 +19,20 @@ function getConfigData() {
 	var toggleShowSecondhand = document.getElementById('toggle-show-secondhand');
 	var toggleShowDate = document.getElementById('toggle-show-date');
 	var toggleShowDropShadow = document.getElementById('toggle-show-drop-shadow');
+	var toggleShowNumber = document.getElementById('toggle-show-number');
+	var toggleShowIndex = document.getElementById('toggle-show-index');
+	var radioTapNone = document.getElementById('radio-tap-none');
+	var radioTapDigital = document.getElementById('radio-tap-digital');
+	var radioTapFlip = document.getElementById('radio-tap-flip');
+	var timeShift = document.getElementById('time-shift');
+	var toggleReversal = document.getElementById('toggle-reversal');
 
 	var options = {
 		'radio-hand-diamond': radioHandDiamond.checked,
 		'radio-hand-trapezoid': radioHandTrapezoid.checked,
 		'radio-hand-rectangle': radioHandRectangle.checked,
+		'radio-hand-pentagon': radioHandPentagon.checked,
+		'radio-hand-round': radioHandRound.checked,
 		'color-hourhand': colorHourhand.value,
 		'color-hourhand-outline': colorHourhandOutline.value,
 		'color-minutehand': colorMinutehand.value,
@@ -35,7 +46,14 @@ function getConfigData() {
 		'color-drop-shadow': colorDropShadow.value,
 		'toggle-show-secondhand': toggleShowSecondhand.checked,
 		'toggle-show-date': toggleShowDate.checked,
-		'toggle-show-drop-shadow': toggleShowDropShadow.checked
+		'toggle-show-drop-shadow': toggleShowDropShadow.checked,
+		'toggle-show-number': toggleShowNumber.checked,
+		'toggle-show-index': toggleShowIndex.checked,
+		'radio-tap-none': radioTapNone.checked,
+		'radio-tap-digital': radioTapDigital.checked,
+		'radio-tap-flip': radioTapFlip.checked,
+		'time-shift': timeShift.value,
+		'toggle-reversal': toggleReversal.checked
 	};
 
 	// Save for next launch
@@ -43,6 +61,8 @@ function getConfigData() {
 	localStorage['radio-hand-diamond'] = options['radio-hand-diamond'];
 	localStorage['radio-hand-trapezoid'] = options['radio-hand-trapezoid'];
 	localStorage['radio-hand-rectangle'] = options['radio-hand-rectangle'];
+	localStorage['radio-hand-pentagon'] = options['radio-hand-pentagon'];
+	localStorage['radio-hand-round'] = options['radio-hand-round'];
 	localStorage['color-hourhand'] = options['color-hourhand'];
 	localStorage['color-hourhand-outline'] = options['color-hourhand-outline'];
 	localStorage['color-minutehand'] = options['color-minutehand'];
@@ -57,6 +77,13 @@ function getConfigData() {
 	localStorage['toggle-show-secondhand'] = options['toggle-show-secondhand'];
 	localStorage['toggle-show-date'] = options['toggle-show-date'];
 	localStorage['toggle-show-drop-shadow'] = options['toggle-show-drop-shadow'];
+	localStorage['toggle-show-number'] = options['toggle-show-number'];
+	localStorage['toggle-show-index'] = options['toggle-show-index'];
+	localStorage['radio-tap-none'] = options['radio-tap-none'];
+	localStorage['radio-tap-digital'] = options['radio-tap-digital'];
+	localStorage['radio-tap-flip'] = options['radio-tap-flip'];
+	localStorage['time-shift'] = options['time-shift'];
+	localStorage['toggle-reversal'] = options['toggle-reversal'];
 
 	console.log('Got options: ' + JSON.stringify(options));
 
@@ -112,6 +139,8 @@ buttonSave.addEventListener('click', function() {
 	var radioHandDiamond = document.getElementById('radio-hand-diamond');
 	var radioHandTrapezoid = document.getElementById('radio-hand-trapezoid');
 	var radioHandRectangle = document.getElementById('radio-hand-rectangle');
+	var radioHandPentagon = document.getElementById('radio-hand-pentagon');
+	var radioHandRound = document.getElementById('radio-hand-round');
 	var colorHourhand = document.getElementById('color-hourhand');
 	var colorHourhandOutline = document.getElementById('color-hourhand-outline');
 	var colorMinutehand = document.getElementById('color-minutehand');
@@ -126,12 +155,21 @@ buttonSave.addEventListener('click', function() {
 	var toggleShowSecondhand = document.getElementById('toggle-show-secondhand');
 	var toggleShowDate = document.getElementById('toggle-show-date');
 	var toggleShowDropShadow = document.getElementById('toggle-show-drop-shadow');
+	var toggleShowNumber = document.getElementById('toggle-show-number');
+	var toggleShowIndex = document.getElementById('toggle-show-index');
+	var radioTapNone = document.getElementById('radio-tap-none');
+	var radioTapDigital = document.getElementById('radio-tap-digital');
+	var radioTapFlip = document.getElementById('radio-tap-flip');
+	var timeShift = document.getElementById('time-shift');
+	var toggleReversal = document.getElementById('toggle-reversal');
 
 	// Load any previously saved configuration, if available
 	if(localStorage['flag_save'] == "1") {
 		radioHandDiamond.checked = JSON.parse(localStorage['radio-hand-diamond']);
 		radioHandTrapezoid.checked = JSON.parse(localStorage['radio-hand-trapezoid']);
 		radioHandRectangle.checked = JSON.parse(localStorage['radio-hand-rectangle']);
+		radioHandPentagon.checked = JSON.parse(localStorage['radio-hand-pentagon']);
+		radioHandRound.checked = JSON.parse(localStorage['radio-hand-round']);
 		colorHourhand.value = localStorage['color-hourhand'];
 		colorHourhandOutline.value = localStorage['color-hourhand-outline'];
 		colorMinutehand.value = localStorage['color-minutehand'];
@@ -146,6 +184,13 @@ buttonSave.addEventListener('click', function() {
 		toggleShowSecondhand.checked = JSON.parse(localStorage['toggle-show-secondhand']);
 		toggleShowDate.checked = JSON.parse(localStorage['toggle-show-date']);
 		toggleShowDropShadow.checked = JSON.parse(localStorage['toggle-show-drop-shadow']);
+		toggleShowNumber.checked = JSON.parse(localStorage['toggle-show-number']);
+		toggleShowIndex.checked = JSON.parse(localStorage['toggle-show-index']);
+		radioTapNone.checked = JSON.parse(localStorage['radio-tap-none']);
+		radioTapDigital.checked = JSON.parse(localStorage['radio-tap-digital']);
+		radioTapFlip.checked = JSON.parse(localStorage['radio-tap-flip']);
+		timeShift.value = localStorage['time-shift'];
+		toggleReversal.checked = JSON.parse(localStorage['toggle-reversal']);
 	}
 
 })();
