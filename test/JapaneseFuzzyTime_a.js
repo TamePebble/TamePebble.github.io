5721761ba3dda67e82fa8b0937662d069a79ba75
TamePebble <tamewatch@gmail.com>
2016-03-16 10:12:25 +0900

    Update JapaneseFuzzyTime_a.js

--- test/JapaneseFuzzyTime_a.js
+++ test/JapaneseFuzzyTime_a.js
@@ -1,1 +1,86 @@
+function getConfigData() {
 
+	var toggleInvert = document.getElementById('toggle-invert');
+	var toggleIconBattery = document.getElementById('toggle-icon-battery');
+	var toggleVibrationTimeSignal = document.getElementById('toggle-vibration-time-signal');
+	var toggleVibrationConnection = document.getElementById('toggle-vibration-connection');
+
+	var options = {
+		'toggle-invert': toggleInvert.checked,
+		'toggle-icon-battery': toggleIconBattery.checked,
+		'toggle-vibration-time-signal': toggleVibrationTimeSignal.checked,
+		'toggle-vibration-connection': toggleVibrationConnection.checked
+	};
+
+	// Save for next launch
+	localStorage['flag_save'] = 1;
+	localStorage['toggle-invert'] = options['toggle-invert'];
+	localStorage['toggle-icon-battery'] = options['toggle-icon-battery'];
+	localStorage['toggle-vibration-time-signal'] = options['toggle-vibration-time-signal'];
+	localStorage['toggle-vibration-connection'] = options['toggle-vibration-connection'];
+
+	console.log('Got options: ' + JSON.stringify(options));
+
+	return options;
+
+}
+
+function getQueryParam(variable, defaultValue) {
+
+	var query = location.search.substring(1);
+	var vars = query.split('&');
+
+	for (var i = 0; i < vars.length; i++) {
+
+		var pair = vars[i].split('=');
+
+		if (pair[0] === variable) {
+			return decodeURIComponent(pair[1]);
+		}
+
+	}
+
+	return defaultValue || false;
+
+}
+
+var buttonCancel = document.getElementById('button-cancel');
+
+buttonCancel.addEventListener('click', function() {
+
+//		console.log('Cancel');
+
+	// Set the return URL depending on the runtime environment
+	var return_to = getQueryParam('return_to', 'pebblejs://close#');
+	document.location = return_to;
+
+});
+
+var buttonSave = document.getElementById('button-save');
+
+buttonSave.addEventListener('click', function() {
+
+//		console.log('Submit');
+
+	// Set the return URL depending on the runtime environment
+	var return_to = getQueryParam('return_to', 'pebblejs://close#');
+	document.location = return_to + encodeURIComponent(JSON.stringify(getConfigData()));
+
+});
+
+(function() {
+
+	var toggleInvert = document.getElementById('toggle-invert');
+	var toggleIconBattery = document.getElementById('toggle-icon-battery');
+	var toggleVibrationTimeSignal = document.getElementById('toggle-vibration-time-signal');
+	var toggleVibrationConnection = document.getElementById('toggle-vibration-connection');
+
+	// Load any previously saved configuration, if available
+	if(localStorage['flag_save'] == "1") {
+		toggleInvert.checked = JSON.parse(localStorage['toggle-invert']);
+		toggleIconBattery.checked = JSON.parse(localStorage['toggle-icon-battery']);
+		toggleVibrationTimeSignal.checked = JSON.parse(localStorage['toggle-vibration-time-signal']);
+		toggleVibrationConnection.checked = JSON.parse(localStorage['toggle-vibration-connection']);
+	}
+
+})();
